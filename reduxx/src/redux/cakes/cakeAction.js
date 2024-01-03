1a08c1da85c591ddae6c6ff3cb16640a960fc285
Aakarshit <rajaakarshit7@gmail.com>
2024-01-03 20:19:05 +0530

    Action payload

--- reduxx/src/redux/cakes/cakeAction.js
+++ reduxx/src/redux/cakes/cakeAction.js
@@ -2,6 +2,6 @@ import { BUY_CAKE } from "./cakeType"
 export const buyCake=(number=1)=>{
     return {
         type:BUY_CAKE,
-        payload:number
+         payload:number,
     }
 }
--- reduxx/src/redux/cakes/cakeReducer.js
+++ reduxx/src/redux/cakes/cakeReducer.js
@@ -1,16 +1,19 @@
-import { BUY_CAKE } from "./cakeType"
-const initialState={
-    numOfCakes:50
-}
+import { BUY_CAKE } from "./cakeType";
 
-const cakeReducer=(state=initialState,action)=>{
-    switch(action.type){
-        case BUY_CAKE:return{
-            ...state,
-            numOfCakes:state.numOfCakes-action.payload
-        }
-        default:return state
-    }
-}
+const initialState = {
+  numOfCakes: 50
+};
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: Math.max(0, state.numOfCakes - action.payload)
+      };
+    default:
+      return state;
+  }
+};
 
 export default cakeReducer;
