0a88829e2337a951fd027e906ce7870c1495bc8b
Aakarshit <rajaakarshit7@gmail.com>
2024-01-03 16:12:51 +0530

    Apply Middleware in app

--- reduxx/src/redux/store.js
+++ reduxx/src/redux/store.js
@@ -1,7 +1,8 @@
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
 import rootReducer from './rootReducer';
 // import cakeReducer from './cakes/cakeReducer'
 // import iceCreamReducer from './icecream/icecreamReducer';
+import logger from 'redux-logger';
 
 const store=createStore(rootReducer)
 
